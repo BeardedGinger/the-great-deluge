9522239d76f937f3b78ed553b44fd09c18ea9553
BeardedGinger <josh@limecuda.com>
2016-03-10 02:39:22 -0500

    home widget style start & add footer back to front-page template

--- front-page.php
+++ front-page.php
@@ -19,11 +19,6 @@ remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 //* Remove site footer widgets
 remove_action( 'genesis_before_footer', 'genesis_footer_widget_areas' );
 
-//* Remove site footer elements
-remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
-remove_action( 'genesis_footer', 'genesis_do_footer' );
-remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
-
 add_action( 'genesis_after_header', 'tgd_hero_section' );
 /**
  * Hero Text & Countdown
